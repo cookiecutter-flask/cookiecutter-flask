612dd55579e6036d46fb3885329568855f6fc4d0
Steven Loria <sloria1@gmail.com>
2013-12-07 18:04:26 -0600

    Remove run.py

--- {{cookiecutter.repo_name}}/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-from {{cookiecutter.repo_name}} import main
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    main.app.run(port=port)
