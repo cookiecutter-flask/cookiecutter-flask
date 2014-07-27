e589ec1d92788f3cbd82e3007ca797e0833b3b50
sloria <sloria1@gmail.com>
2014-07-27 19:41:00 -0400

    Add abstract base factory

--- {{cookiecutter.app_name}}/tests/factories.py
+++ {{cookiecutter.app_name}}/tests/factories.py
@@ -5,13 +5,19 @@ from factory.alchemy import SQLAlchemyModelFactory
 from {{cookiecutter.app_name}}.user.models import User
 from {{cookiecutter.app_name}}.database import db
 
+class BaseFactory(SQLAlchemyModelFactory):
 
-class UserFactory(SQLAlchemyModelFactory):
     class Meta:
-        model = User
+        abstract = True
         sqlalchemy_session = db.session
 
+
+class UserFactory(BaseFactory):
     username = Sequence(lambda n: "user{0}".format(n))
     email = Sequence(lambda n: "user{0}@example.com".format(n))
     password = PostGenerationMethodCall('set_password', 'example')
     active = True
+
+    class Meta:
+        model = User
+
