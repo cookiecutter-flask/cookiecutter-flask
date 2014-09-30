ccf58472c8027f5e6b19f61bf39b923935e76b7c
sloria <sloria1@gmail.com>
2014-09-29 23:43:40 -0400

    Fix travis.yml; use py.test and install dev-requirements
    Fixes #25

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings.py
@@ -4,7 +4,7 @@ import os
 os_env = os.environ
 
 class Config(object):
-    SECRET_KEY = os_env['{{cookiecutter.app_name | upper}}_SECRET']  # TODO: Change me
+    SECRET_KEY = os_env.get('{{cookiecutter.app_name | upper}}_SECRET', 'secret-key')  # TODO: Change me
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
