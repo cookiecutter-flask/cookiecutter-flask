4c856777f16dbd7926e8b7ca216ecc2dd23767b5
Jeremy Epstein <jazepstein@gmail.com>
2016-09-10 13:36:21 +1000

    Use FLASK_DEBUG instead of MYFLASKAPP_ENV
    Use the new FLASK_DEBUG environment variable
    which is standard as of Flask 0.11, in order
    to control the use of dev or prod config.
    
    Need to set this anyway, in order for "flask run"
    to use reloader and no concurrency.

--- {{cookiecutter.app_name}}/autoapp.py
+++ {{cookiecutter.app_name}}/autoapp.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
-import os
+from flask.helpers import get_debug_flag
 
 from {{cookiecutter.app_name}}.app import create_app
 from {{cookiecutter.app_name}}.settings import DevConfig, ProdConfig
 
-CONFIG = ProdConfig if os.environ.get('{{cookiecutter.app_name | upper}}_ENV') == 'prod' else DevConfig
+CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
