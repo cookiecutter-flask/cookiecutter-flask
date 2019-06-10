17e09a3f61f84cf8d0d7115b8f8562d89542ce21
James Curtin <jameswcurtin@gmail.com>
2019-06-09 21:50:07 -0400

    Remove unused ANSI escape codes

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,14 +6,9 @@ MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
 
 class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKGREEN = "\033[92m"
     WARNING = "\033[93m"
-    FAIL = "\033[91m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
 
 
 def validate_python_module_name():
