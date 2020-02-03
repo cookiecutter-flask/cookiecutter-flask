3c07c42eb9c9ef9498f10f9ea8ef4f71a4310d80
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-02-03 19:56:39 +0100

    fixup! fixup! fixup! Apply suggestions from code review

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings.py
@@ -21,7 +21,6 @@ DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-WEBPACK_MANIFEST_PATH = "webpack/manifest.json"
 {%- if cookiecutter.use_google_signin == "yes" %}
 GOOGLE_CLIENT_ID = env.str("GOOGLE_CLIENT_ID", default=None)
 GOOGLE_CLIENT_SECRET = env.str("GOOGLE_CLIENT_SECRET", default=None)
